5f4b81f04334c309f339af27f6557672eb7744cf
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-05-09 16:14:30 +0400

    Добавьте файлы проекта.

---
+++ AiSD-lab3-2024.cpp
@@ -0,0 +1,12 @@
+﻿// AiSD-lab3-2024.cpp: определяет точку входа для приложения.
+//
+
+#include "AiSD-lab3-2024.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
