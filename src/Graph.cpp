2fc35b2c95fbbdcdcf0a61187b511462dd105b70
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-05-09 16:33:19 +0400

    copied class from instructions

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,8 +1,44 @@
-﻿// AiSD-lab3-2024.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <limits>
+#include <algorithm>
+#include <stdexcept>
 
-// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+template<typename Vertex, typename Distance = double>
+class Graph {
+public:
+	struct {
+		Vertex from;
+		Vertex to;
+		Distance distance;
+	};
+    //проверка-добавление-удаление вершин
+    bool has_vertex(const Vertex& v) const;
+    void add_vertex(const Vertex& v);
+    bool remove_vertex(const Vertex& v);
+    std::vector<Vertex> vertices() const;
+
+
+    //проверка-добавление-удаление ребер
+    void add_edge(const Vertex& from, const Vertex& to, const Distance& d);
+    bool remove_edge(const Vertex& from, const Vertex& to);
+    bool remove_edge(const Edge& e); //c учетом расстояния
+    bool has_edge(const Vertex& from, const Vertex& to) const;
+    bool has_edge(const Edge& e) const; //c учетом расстояния в Edge
+
+    //получение всех ребер, выходящих из вершины
+    std::vector<Edge> edges(const Vertex& vertex);
+
+    size_t order() const; //порядок 
+    size_t degree(const Vertex& v) const; //степень вершины
+
+
+    //поиск кратчайшего пути
+    std::vector<Edge> shortest_path(const Vertex& from,
+        const Vertex& to) const;
+    //обход
+    std::vector<Vertex>  walk(const Vertex& start_vertex)const;
+};
+
+};
